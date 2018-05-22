aea051c54d2d33099165b598c6130152f339b88b
Lesli Villa-Solorzano <leslie.villa10@gmail.com>
2018-05-22 09:07:00 -0400

    add in this tom foolery

---
+++ pquiz.js
@@ -0,0 +1,31 @@
+function clickFunction() {
+var points=0;
+  var choice1= document.querySelector('input[name="q1"]:checked').value;
+  if(choice1 == "Sabine Ulibarrí") {
+    points +=1;
+  }
+  else if (choice1 == "Caillou"){
+    points +=2;
+  }
+  else if(choice1 == "Dora the Explorer"){
+    points +=3;
+  }
+  else if (choice1 == "Curious George"){
+  points+=4;
+}
+if(points ==1){
+  document.getElementById("demo").innerHTML = "Correct!";
+}
+if(points ==2){
+  alert("wrong ");
+}
+if(points ==3){
+  alert("Dora");
+}
+if(points ==4){
+  alert("CG");
+}
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+}
